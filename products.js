cdc3a808d53874c3ce0b43a0465a6cea8e62f4fd
blueyottle <sunnydowari@gmail.com>
2022-11-30 20:40:19 +0530

    initial commit

---
+++ products.js
@@ -0,0 +1,61 @@
+const products = [
+    {
+        id: 1,
+        name: "T-shirt 1",
+        price: 699,
+        instock: 100,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t1.png",
+    },
+    {
+        id: 2,
+        name: "T-shirt 2",
+        price: 699,
+        instock: 100,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t1.png",
+    },
+    {
+        id: 3,
+        name: "T-shirt 3",
+        price: 699,
+        instock: 100,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t1.png",
+    },
+    {
+        id: 4,
+        name: "T-shirt 4",
+        price: 699,
+        instock: 100,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t1.png",
+    },
+    {
+        id: 5,
+        name: "T-shirt 5",
+        price: 699,
+        instock: 100,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t1.png",
+    },
+    {
+        id: 6,
+        name: "T-shirt 6",
+        price: 699,
+        instock: 100,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./img/t1.png",
+    },
+
+
+
+
+
+]
